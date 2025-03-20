8891b3ade8bcf0750e1514bf194f3630f87c5d32
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-03-20 20:57:26 +0700

    membuat fungsi untuk menambah data todo

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -12,7 +12,23 @@ class _TodoPageState extends State<TodoPage> {
   final TextEditingController _taskController = TextEditingController();
   final TextEditingController _dateController = TextEditingController();
   List<Map<String, dynamic>> listTugas = [];
-  
+
+  void addData() {
+    if (_key.currentState!.validate()) {
+      setState(() {
+        listTugas.add({
+          'task': _taskController.text,
+          'date': _dateController.text,
+          'done': false,
+        });
+
+        // Kosongkan input setelah submit
+        _taskController.clear();
+        _dateController.clear();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
